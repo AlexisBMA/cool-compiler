77e47fe3095cc0bd24b8504dfdf60ac30ee70f2d
Abdul <abdul@tec.mx>
2022-03-29 15:54:43 -0600

    LLamar al addressing listener

--- ejemplosANTLR/marzo/client.py
+++ ejemplosANTLR/marzo/client.py
@@ -1,11 +1,12 @@
 from antlr4 import *
 from antlr.marzoParser import marzoParser
 from antlr.marzoLexer import marzoLexer
+
 from listeners.datasegment import DataGenerator
+from listeners.addressing import Addressing
 from listeners.gencode import GenCode
 
 import sys
-
 
 def main():
     parser = marzoParser(CommonTokenStream(marzoLexer(FileStream("input.txt"))))
@@ -17,6 +18,7 @@ def main():
     walker = ParseTreeWalker()
     
     # Por las constantes, ahora dataGen debe ir ANTES
+    walker.walk(Addressing(), tree)
     walker.walk(dataGen, tree)
     walker.walk(gencode, tree)
 
