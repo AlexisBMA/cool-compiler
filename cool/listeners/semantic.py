f8d03737a8acd9530840f152cb3052ac9fb2ed5f
AlexisBMA <a01026848@itesm.mx>
2022-04-28 09:39:51 -0500

    Semantic one test finished

--- cool/listeners/semantic.py
+++ cool/listeners/semantic.py
@@ -4,7 +4,7 @@ from antlr.coolParser import coolParser
 from util.structure import * 
 from util.structure import _allClasses as classDict
 
-prohibitedClassnames = {'Int': badredefineint}
+prohibitedClassnames = {'Int': badredefineint, 'Object': redefinedobject, 'SELF_TYPE': selftyperedeclared}
 prohibitedInheritance = {'Bool': inheritsbool, 'String': inheritsstring, 'SELF_TYPE': inheritsselftype}
 class semanticListener(coolListener):
 
@@ -16,7 +16,6 @@ class semanticListener(coolListener):
 
         if className in prohibitedClassnames:
             raise prohibitedClassnames[className]()
-        
         if ctx.TYPE(1):
             classInherits = ctx.TYPE(1).getText()
             if classInherits in prohibitedInheritance:
@@ -34,10 +33,25 @@ class semanticListener(coolListener):
         if featureID == 'self' or featureID == 'SELF_TYPE':
             raise anattributenamedself("Feature ID not valid (self)")
 
+        if ctx.expr():
+            if ctx.expr().ID():
+                if ctx.expr().ID().getText() == 'self':
+                    raise selfassignment('Assignment to self is prohibited')
+
+            if ctx.formal():
+                if ctx.formal(0).ID().getText() == 'self':
+                    raise selfinformalparameter('Using self as a parameter is prohibited')
+                if ctx.formal(0).TYPE().getText() == 'SELF_TYPE':
+                    raise selftypeparameterposition('SELF_TYPE cannot be used as a parameter type')
+
+            expr = ctx.expr().getText()
+            if ctx.expr().let_decl():
+                let_params = ctx.expr().let_decl(0)
+                if let_params.ID().getText() == 'self' or let_params.ID().getText() == 'SELF_TYPE':
+                    raise letself("Let incorrect (using self)")
+
 
 
     def exitKlass(self, ctx:coolParser.KlassContext):
         if (not self.main):
             raise nomain()
-
-    
